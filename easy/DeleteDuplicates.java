af373d51b62c311ba7813b87faef55a15afa4ef1
Bassama Paul Hervé <bphsam@gmail.com>
2017-12-13 17:42:21 +0100

    Update DeleteDuplicates.java
    Add a correction for this solution

--- easy/DeleteDuplicates.java
+++ easy/DeleteDuplicates.java
@@ -16,3 +16,15 @@ public ListNode deleteDuplicates(ListNode head) {
     }
     return head;
 }
+
+/** corrections */
+public ListNode deleteDuplicates(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode current = head;
+    while (current.next != null) {
+        if (current.val == current.next.val)
+            current.next = current.next.next;
+        else current = current.next;
+    }
+    return head;
+}
