4fb69e0e446fbb4332890ed127e9a6b4ab2c0d81
Bassama Paul Hervé <bphsam@gmail.com>
2017-12-13 15:46:58 +0100

    Update RemoveElement.java

--- easy/RemoveElement.java
+++ easy/RemoveElement.java
@@ -22,3 +22,20 @@ public int removeElement(int[] nums, int val) {
     }
     return M;
 }
+
+public int removeElement(int[] nums, int val) {
+    if (nums == null || nums.length == 0) {
+        return 0;
+    }
+
+    int n = nums.length;
+    int k = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != val) {
+            nums[++k] = nums[i];
+        }
+    }
+
+    return k + 1;
+}
