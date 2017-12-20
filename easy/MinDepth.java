7a162dcc32d729c9e6475097dbd41526854d21cf
Bassama Paul Hervé <bphsam@gmail.com>
2017-12-20 13:42:19 +0100

    Update MinDepth.java

--- easy/MinDepth.java
+++ easy/MinDepth.java
@@ -59,3 +59,19 @@ public int minDepth(TreeNode root) {
     
     return 0;
 }
+
+/** other solution 20/12/2017 */
+public int minDepth(TreeNode root) {
+    if (root == null) {
+        return 0;
+    }
+    if (root.left == null && root.right == null) {
+        return 1;
+    } else if (root.left != null && root.right != null) {
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    } else if (root.left != null) {
+        return 1 + minDepth(root.left);
+    } else {
+        return 1 + minDepth(root.right);
+    }
+}
