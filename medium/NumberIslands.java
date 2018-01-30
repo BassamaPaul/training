5d4e761b56ffe60679c1cad89595d8bf34f21166
Bassama Paul Hervé <bphsam@gmail.com>
2018-01-30 15:20:37 +0100

    Create NumberIslands.java

---
+++ medium/NumberIslands.java
@@ -0,0 +1,49 @@
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+Answer: 3
+*/
+
+public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        return 0;
+    }
+
+    int n = grid.length;
+    int m = grid[0].length;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == '0') continue;
+            count++;
+            dfs(grid, i, j);
+        }
+    }
+    return count;
+}
+
+public void dfs(char[][] grid, int i, int j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
+}
